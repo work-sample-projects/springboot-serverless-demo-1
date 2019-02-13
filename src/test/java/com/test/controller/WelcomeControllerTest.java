c22bda6c1824cd45dca69eafbf9181acbdd8b75b
Michael Welte <lysander655@gmail.com>
2019-02-13 15:49:27 -0500

    Cleaned up the test file to remove unused imports and other minor cleanup

--- src/test/java/com/test/controller/WelcomeControllerTest.java
+++ src/test/java/com/test/controller/WelcomeControllerTest.java
@@ -6,24 +6,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import com.test.driver.SpringBootWebApplication;
-
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(WelcomeController.class)
-
-//@AutoConfigureMockMvc
-//@SpringBootTest(classes = WelcomeController.class)
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { WelcomeController.class })
