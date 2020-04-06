d8a20a1b359a2ab3c6f08457357908b247870b58
bassdude5 <lysander655@gmail.com>
2020-04-05 22:09:18 -0400

    Updated scripts

--- src/scripts/buildah.sh
+++ src/scripts/buildah.sh
@@ -3,4 +3,4 @@ echo "Building docker image using: "
 buildah --version
 echo ""
 
-buildah bud -t springboot-serverless-demo-0.0.1 .
+buildah bud -t springboot-serverless-demo-1.0.0 .
--- src/scripts/podman.sh
+++ src/scripts/podman.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Running image using podman"
 
+podman run -p 8080:8080 springboot-serverless-demo-1.0.0
